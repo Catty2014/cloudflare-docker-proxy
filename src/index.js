065f8d4a7d5e2fc095278cc429b020686f0210c5
CattySteve <cattysteve89265@163.com>
2024-06-20 12:35:16 +0800

    Modified by Catty

--- src/index.js
+++ src/index.js
@@ -9,16 +9,10 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
-
-  // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "dockerworker.cattysteve.top": "https://registry-1.docker.io",
+  "gcrworker.cattysteve.top": "https://gcr.io",
+  "k8sworker.cattysteve.top": "https://registry.k8s.io",
+  "ghcrworker.cattysteve.top": "https://ghcr.io",
 };
 
 function routeByHosts(host) {
